fe5b6c11b1df64a50a458f3ee2c12f98baab0888
angelabrand <165895780+angelabrand@users.noreply.github.com>
2024-10-08 13:00:24 +0200

    Modulo 8

--- Modulo 7/src/motor.spec.ts
+++ Modulo 7/src/motor.spec.ts
@@ -1,8 +1,12 @@
 import { vi } from "vitest";
 import { obtenerNumeroAleatorio } from "./modelo";
-import {obtenerPuntosCarta, sumarPuntosCarta, marcadorPuntuacion, actualizarPuntuacion, marcardorACero} from "./motor";
-
-
+import {
+  obtenerPuntosCarta,
+  sumarPuntosCarta,
+  marcadorPuntuacion,
+  actualizarPuntuacion,
+  marcardorACero,
+} from "./motor";
 
 describe("generarNumeroAleatorio", () => {
   it("MathRandom forzado a 0 y deberia dar 0", () => {
@@ -29,73 +33,67 @@ describe("generarNumeroAleatorio", () => {
   });
 });
 
-
-
-describe("valor del marcador reseteado" , () => {
-  it('deberia de ser 0', () => {
+describe("valor del marcador reseteado", () => {
+  it("deberia de ser 0", () => {
     //arrange
 
-   const resultadoEsperado: number= 0
+    const resultadoEsperado: number = 0;
     //act
-    const resultado = marcardorACero()
+    const resultado = marcardorACero();
 
     //assert
-    expect(resultado).toBe(resultadoEsperado)
-  })
-})
+    expect(resultado).toBe(resultadoEsperado);
+  });
+});
 
-
-describe("Valor de las cartas " , () => {
-  it('si es una carta del 1 al 7 deberia obtener 1 punto', () => {
+describe("Valor de las cartas ", () => {
+  it("si es una carta del 1 al 7 deberia obtener 1 punto", () => {
     //arrange
-   const carta: number = 2
-   const resultadoEsperado: number = 3
+    const carta: number = 2;
+    const resultadoEsperado: number = 3;
     //act
-    const resultado = obtenerPuntosCarta(carta)
+    const resultado = obtenerPuntosCarta(carta);
 
     //assert
-    expect(resultado).toBe(resultadoEsperado)
-  })
-  it('si es sota, caballo o rey deberia obtener 0.5 puntos', () => {
+    expect(resultado).toBe(resultadoEsperado);
+  });
+  it("si es sota, caballo o rey deberia obtener 0.5 puntos", () => {
     //arrange
-   const carta: number = 8
-   const resultadoEsperado: number = 0.5
+    const carta: number = 8;
+    const resultadoEsperado: number = 0.5;
     //act
-    const resultado = obtenerPuntosCarta(carta)
+    const resultado = obtenerPuntosCarta(carta);
 
     //assert
-    expect(resultado).toBe(resultadoEsperado)
-  })
-})
+    expect(resultado).toBe(resultadoEsperado);
+  });
+});
 
-describe('registro de los puntos en el marcadorPuntuacion', 
-  () => {
-    it('que se sumen los 1 puntos al marcadorPuntuacion', () => {
-      const puntos : number = 2
-      const sumaAlMacador : number = marcadorPuntuacion + 2
+describe("registro de los puntos en el marcadorPuntuacion", () => {
+  it("que se sumen los 1 puntos al marcadorPuntuacion", () => {
+    const puntos: number = 2;
+    const sumaAlMacador: number = marcadorPuntuacion + 2;
 
-      const resultado = sumarPuntosCarta(puntos)
+    const resultado = sumarPuntosCarta(puntos);
 
-      expect(resultado).toBe(sumaAlMacador)
-    })
-    it('que se sumen los puntos 0.5 al marcadorPuntuacion', () => {
-      const puntos : number = 1.5
-      const sumaAlMacador : number = marcadorPuntuacion + puntos
+    expect(resultado).toBe(sumaAlMacador);
+  });
+  it("que se sumen los puntos 0.5 al marcadorPuntuacion", () => {
+    const puntos: number = 1.5;
+    const sumaAlMacador: number = marcadorPuntuacion + puntos;
 
-      const resultado = sumarPuntosCarta(puntos)
+    const resultado = sumarPuntosCarta(puntos);
 
-      expect(resultado).toBe(sumaAlMacador)
-    })
-  }
-)
+    expect(resultado).toBe(sumaAlMacador);
+  });
+});
 
-describe('Actualizar puntuacion', ()=> {
-  it('que marcadorPuntuacion deberia mostrar los puntos actualizados' , () => {
-   
-    const puntosActuales :number = 3
-    
-  actualizarPuntuacion(puntosActuales)
+describe("Actualizar puntuacion", () => {
+  it("que marcadorPuntuacion deberia mostrar los puntos actualizados", () => {
+    const puntosActuales: number = 3;
 
-    expect(marcadorPuntuacion).toBe(puntosActuales)
-  })
-})      
+    actualizarPuntuacion(puntosActuales);
+
+    expect(marcadorPuntuacion).toBe(puntosActuales);
+  });
+});
---
+++ Modulo 8/src/main.ts
@@ -0,0 +1,90 @@
+type Especialidad = "Medico de familia" | "Pediatra" | "Cardiólogo";
+
+interface Pacientes {
+  id: number;
+  nombre: string;
+  apellidos: string;
+  sexo: string;
+  temperatura: number;
+  frecuenciaCardiaca: number;
+  especialidad: Especialidad;
+  edad: number;
+}
+
+const pacientes: Pacientes[] = [
+  {
+    id: 1,
+    nombre: "John",
+    apellidos: "Doe",
+    sexo: "Male",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 80,
+    especialidad: "Medico de familia",
+    edad: 44,
+  },
+  {
+    id: 2,
+    nombre: "Jane",
+    apellidos: "Doe",
+    sexo: "Female",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 70,
+    especialidad: "Medico de familia",
+    edad: 43,
+  },
+  {
+    id: 3,
+    nombre: "Junior",
+    apellidos: "Doe",
+    sexo: "Male",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 90,
+    especialidad: "Pediatra",
+    edad: 8,
+  },
+  {
+    id: 4,
+    nombre: "Mary",
+    apellidos: "Wien",
+    sexo: "Female",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 120,
+    especialidad: "Medico de familia",
+    edad: 20,
+  },
+  {
+    id: 5,
+    nombre: "Scarlett",
+    apellidos: "Somez",
+    sexo: "Female",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 110,
+    especialidad: "Cardiólogo",
+    edad: 30,
+  },
+  {
+    id: 6,
+    nombre: "Brian",
+    apellidos: "Kid",
+    sexo: "Male",
+    temperatura: 39.8,
+    frecuenciaCardiaca: 80,
+    especialidad: "Pediatra",
+    edad: 11,
+  },
+];
+
+
+
+// Queremos extraer la lista de paciente que están asignados a la especialidad de Pediatría
+
+const obtenPacientesAsignadosAPediatria = (pacientes: Pacientes[]): Pacientes[] => {
+
+}
+
+const todosLosPacientes = obtenPacientesAsignadosAPediatria(pacientes)
+console.log(todosLosPacientes)
+
+//Queremos extraer la lista de pacientes asignados a Pediatría y que tengan una edad menor de 10 años.
+
+
