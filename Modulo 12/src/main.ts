2ae6a649e586d886775a6f0f496c34907ed491a6
angelabrand <angelamolinagui@gmail.com>
2026-01-02 18:46:09 +0100

    desafio

--- Modulo 12/src/main.ts
+++ Modulo 12/src/main.ts
@@ -168,3 +168,75 @@ class tourOperador extends total {
 const tourOperadorDeReservas = new tourOperador(reservasClientes, 0.15);
 console.log("Subtotal:", tourOperadorDeReservas.calcularSubtotal());
 console.log("Total:", tourOperadorDeReservas.calcularTotal());
+
+class base {
+  reservas: Reserva[];
+  cargoAdicional: number;
+  IVA: number;
+  precioSuite: number;
+  precioStandard: number;
+
+  constructor(
+    reservas: Reserva[],
+    precioSuite: number,
+    precioStandard: number
+  ) {
+    this.reservas = reservas;
+    this.cargoAdicional = 40;
+    this.IVA = 1.21;
+    this.precioSuite = precioSuite;
+    this.precioStandard = precioStandard;
+  }
+  calcularSubtotal() {
+    let subtotal = 0;
+    this.reservas.forEach((reserva) => {
+      let totalReserva = 0;
+      if (reserva.tipoHabitacion == "standard") {
+        totalReserva = reserva.noches * this.precioStandard;
+      }
+      if (reserva.tipoHabitacion == "suite") {
+        totalReserva = reserva.noches * this.precioSuite;
+      }
+      if (reserva.pax > 1) {
+        totalReserva +=
+          reserva.noches * (reserva.pax - 1) * this.cargoAdicional;
+      }
+      subtotal += totalReserva;
+    });
+    return subtotal;
+  }
+  calcularTotal() {
+    return this.calcularSubtotal() * this.IVA;
+  }
+}
+
+class Tour extends base {
+  descuento: number;
+  constructor(reservas: Reserva[]) {
+    super(reservas, 100, 100);
+    this.descuento = 0.15;
+  }
+  calcularTotal(): number {
+    return (
+      (this.calcularSubtotal() - this.calcularSubtotal() * this.descuento) *
+      this.IVA
+    );
+  }
+}
+
+class Particular extends base {
+  constructor(reservas: Reserva[]) {
+    super(reservas, 100, 120);
+  }
+}
+
+let instanciaBase: base = new base(reservasClientes, 100, 120);
+const paquitosAmigos: Tour = new Tour(reservasClientes);
+const JoseAntonio: Particular = new Particular(reservasClientes);
+
+console.log(instanciaBase.calcularSubtotal());
+console.log(instanciaBase.calcularTotal());
+console.log(paquitosAmigos.calcularSubtotal());
+console.log(paquitosAmigos.calcularTotal());
+console.log(JoseAntonio.calcularSubtotal());
+console.log(JoseAntonio.calcularTotal());
